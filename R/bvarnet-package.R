b1ac62eee9fb02896da109f79595d70b436678b3
flo1met <152606616+flo1met@users.noreply.github.com>
2026-04-07 21:47:00 +0200

    small fixes

--- R/bvarnet-package.R
+++ R/bvarnet-package.R
@@ -3,6 +3,6 @@
 
 ## usethis namespace: start
 #' @importFrom instantiate stan_package_model
-#' @importFrom stats complete.cases quantile rbinom rnorm runif sd
+#' @importFrom stats complete.cases quantile rbinom rnorm runif sd setNames
 ## usethis namespace: end
 NULL
