3ced0548ef937c714f2d538c8aee366b35a5d4d4
flo1met <152606616+flo1met@users.noreply.github.com>
2026-04-14 14:08:02 +0200

    edit vignetes and data

---
+++ R/studentlife.R
@@ -0,0 +1,10 @@
+#' StudentLife Data
+#'
+#' Assessing mental health, academic performance and behavioral trends of college students using smartphones.
+#'
+#' @format ## `studentlife`
+#' A data frame with 1912 rows and 72 columns:
+
+#' @source https://openesmdata.org/datasets/0004_wang/
+#' @source https://doi.org/10.1145/2632048.2632054
+"studentlife"
---
+++ data-raw/studentlife.R
@@ -0,0 +1,20 @@
+## code to prepare `studentlife` dataset goes here
+
+# Wang, R., Chen, F., Chen, Z., Li, T., Harari, G., Tignor, S., Zhou, X., Ben-Zeev, D., & Campbell, A. T. (2014).
+# StudentLife: assessing mental health, academic performance and behavioral trends of college students using smartphones.
+# Proceedings of the 2014 ACM International Joint Conference on Pervasive and Ubiquitous Computing, 3–14.
+# https://doi.org/10.1145/2632048.2632054
+
+library(openesm)
+
+data <- get_dataset("0004")
+
+studentlife <- data$data
+
+studentlife$anxious <- floor(studentlife$anxious)
+studentlife$calm <- floor(studentlife$calm)
+studentlife$conventional <- floor(studentlife$conventional)
+studentlife$critical <- floor(studentlife$critical)
+studentlife$dependable <- floor(studentlife$dependable)
+
+usethis::use_data(studentlife, overwrite = TRUE)
